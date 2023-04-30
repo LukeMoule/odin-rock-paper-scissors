b2adadcf1eb45b9b02a8e96aa098651a731c8b2b
Luke Moule <65358800+LukeMoule@users.noreply.github.com>
2023-05-01 00:15:20 +0100

    implemented function to play round of rps

--- javascript.js
+++ javascript.js
@@ -1,1 +1,42 @@
-console.log("Hello World"); 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getComputerChoice() {
+    let value = Math.floor(Math.random() * 3);
+    switch (value){
+        case 0: return "rock";
+        case 1: return "paper";
+        case 2: return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection){
+        return `Draw. You both picked ${playerSelection}.`;
+    }
+
+    let win;
+
+    switch(playerSelection){
+        case "rock":{
+            win = computerSelection === "scissors";
+            break;
+        }
+        
+        case "paper":{
+            win = computerSelection === "rock";
+            break;
+        }
+
+        case "scissors":{
+            win = computerSelection === "paper";
+            break;
+        }
+    }
+
+    let output = `You ${win ? "win!" : "lose!"} ${capitalizeFirstLetter(playerSelection)} ${win ? "beats" : "loses to"} ${computerSelection}.`;
+    return output;
+}
